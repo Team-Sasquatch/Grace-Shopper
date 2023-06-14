6e09ca8243f0181c1d8d6953efc39d8a19f8626f
daventsai <daventsai75@gmail.com>
2023-06-14 10:37:16 -0500

    initial table creation

--- db/seed.js
+++ db/seed.js
@@ -10,6 +10,35 @@ async function dropTables() {
 
 async function createTables() {
   console.log("Creating tables...");
+  await client.query(`
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL
+    );
+    CREATE TABLE orders(
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(id),
+      cost INTEGER NOT NULL,
+      order_number varchar(255) UNIQUE NOT NULL
+    );
+    CREATE TABLE products(
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL,
+      price INTEGER,
+      description varchar(255)
+    );
+    CREATE TABLE orders_products(
+      id SERIAL PRIMARY KEY,
+      order_id INTEGER REFERENCES orders(id),
+      product_id INTEGER REFERENCES products(id)
+    )
+    CREATE TABLE sports(
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL,
+      description varchar(255)
+    )
+  `)
   try {
   } catch (error) {
     console.log(error);
