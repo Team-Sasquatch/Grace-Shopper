5d602c043fdad7ea2d89be24f98ad41346f18ff0
Daniel H <dannyhdz27@gmail.com>
2023-06-15 11:17:47 -0500

    working on products route

--- routes/products.js
+++ routes/products.js
@@ -1,3 +1,14 @@
+const { getAllProducts } = require("../db/adapters/products");
+
 const productsRouter = require("express").Router();
 
-productsRouter.get("/", (req, res, next) => {});
+productsRouter.get("/", async (req, res, next) => {
+  try {
+    const products = await getAllProducts();
+    res.send(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = productsRouter;
