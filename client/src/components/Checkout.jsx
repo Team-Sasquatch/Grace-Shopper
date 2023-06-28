4585ffd1a3d665b7c35ac73e0e48d12b5b5fd68e
daventsai <daventsai75@gmail.com>
2023-06-28 10:25:56 -0500

    checkout working

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -3,16 +3,32 @@ import useAuth from "../hooks/useAuth";
 
 export default function Checkout(){
     const [quantity,setQuantity]=useState(null);
-
+    const [retrievedCart,setRetrievedCart]=useState([]);
     useEffect(()=>{
-        const retrievedOrder = localStorage.getItem('testObj');//update name after Sarah creates the add to cart functionality
+        setRetrievedCart(JSON.parse(localStorage.getItem('shoppingCart')));
 
     },[])
-
+    console.log('retrievedCart',retrievedCart)
+    const cartDisplay = retrievedCart;
     return(
         <div>
+            {cartDisplay.length>0
+            ?
             <div>
+                <div>
+                    {cartDisplay.map((prod)=>{
+                        return(
+                            <div>
+                                <p>Product: {prod.name}</p>
+                                <p>Quantity: {prod.quantity}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
+            :
+            <div/>
+            }
         </div>
     )
 }
--- client/src/components/Supplements.jsx
+++ client/src/components/Supplements.jsx
@@ -14,15 +14,32 @@ const SupplementsComponent = () => {
 
   console.log("supps", supplements);
 
+  function Supp(props){
+    var supQty = props.quantity;
+    return(
+      <div key={props.index}>
+        <h1>Name: {props.supplement.name}</h1>
+        <p>Price: {props.supplement.price}</p>
+        <p>Description: {props.supplement.description}</p>
+        <p>Flavor: {props.supplement.flavor}</p>
+        <button onClick={()=>{addToCart({id:props.supplement.id,name:props.supplement.name,quantity:supQty})}}>Add to Cart</button>
+        <p>Quantity: <input type='text' name='quantity' defaultValue={props.quantity} onChange={(e)=>{e.target.value,supQty=parseInt(e.target.value)}}/></p>
+      </div>
+      
+    )
+  }
+
+  function addToCart(cartObj){
+    console.log('cartObject: ',cartObj)
+    let obj = JSON.parse(localStorage.getItem('shoppingCart')) || [];
+    obj.push(cartObj);
+    localStorage.setItem('shoppingCart',JSON.stringify(obj));
+  }
+
   return (
     <div>
       {supplements.map((supp, idx) => (
-        <div key={idx}>
-          <h1>Name: {supp.name}</h1>
-          <p>Price: {supp.price}</p>
-          <p>Description: {supp.description}</p>
-          <p>Flavor: {supp.flavor}</p>
-        </div>
+        <Supp quantity={1} index={idx} supplement={supp}/>
       ))}
     </div>
   );
