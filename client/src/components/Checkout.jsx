6ccace98211f987c28ead5a9c7c0b781e7ee3947
Daniel H <dannyhdz27@gmail.com>
2023-06-29 11:36:19 -0500

    fixed error in checkout.jsx

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -2,45 +2,53 @@ import { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
-export default function Checkout(){
-    const [quantity,setQuantity]=useState(null);
-    const [retrievedCart,setRetrievedCart]=useState([]);
-    const nav = useNavigate();
-    useEffect(()=>{
-        setRetrievedCart(JSON.parse(localStorage.getItem('shoppingCart')));
+export default function Checkout() {
+  const [quantity, setQuantity] = useState(null);
+  const [retrievedCart, setRetrievedCart] = useState([]);
+  const nav = useNavigate();
+  useEffect(() => {
+    setRetrievedCart(JSON.parse(localStorage.getItem("shoppingCart")));
+  }, []);
+  console.log("retrievedCart", retrievedCart);
+  const cartDisplay = retrievedCart;
 
-    },[])
-    console.log('retrievedCart',retrievedCart)
-    const cartDisplay = retrievedCart;
-    
-    function contCheckout(){
-        console.log('cartDisplay',cartDisplay);
-        localStorage.setItem("shoppingCart",JSON.stringify(cartDisplay));
-        nav('/'); //replace with whatever is order confirm page
-    }
+  function contCheckout() {
+    console.log("cartDisplay", cartDisplay);
+    localStorage.setItem("shoppingCart", JSON.stringify(cartDisplay));
+    nav("/"); //replace with whatever is order confirm page
+  }
 
-    return(
+  return (
+    <div>
+      {cartDisplay ? (
         <div>
-            {cartDisplay
-            ?
-            <div>
-                <button onClick={()=>contCheckout()}>Checkout</button>
+          <button onClick={() => contCheckout()}>Checkout</button>
+          <div>
+            {cartDisplay.map((prod) => {
+              return (
                 <div>
-                    {cartDisplay.map((prod)=>{
-                        return(
-                            <div>
-                                <p>Product: {prod.name}</p>
-                                <p>Quantity: <input type='text' defaultValue={prod.quantity}onChange={(e)=>{e.target.value,cartDisplay[prod.id-1].quantity=parseInt(e.target.value)}}/></p>
-                            </div>
-                        )
-                    })}
+                  <p>Product: {prod.name}</p>
+                  <p>
+                    Quantity:{" "}
+                    <input
+                      type="text"
+                      defaultValue={prod.quantity}
+                      onChange={(e) => {
+                        e.target.value,
+                          (cartDisplay[prod.id - 1].quantity = parseInt(
+                            e.target.value
+                          ));
+                      }}
+                    />
+                  </p>
                 </div>
-            </div>
-            :
-             <div className="empty-cart">
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="empty-cart">
           <p>Your cart is empty.</p>
-            </div>
-            }
         </div>
       )}
     </div>
